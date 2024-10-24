f0118d696c4381f2287d3b79cc9da78bb8370475
Steven Ray <95512683+sutibn@users.noreply.github.com>
2024-10-23 20:51:21 -0700

    Added gfx, changed logo

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ with open('./model/model.bin', 'rb') as f_in:
     (dv, model) = pickle.load(f_in)
 feature_input = dict()
 
-st.set_page_config(page_title="Shroom ID", page_icon="./images/brown.ico", layout="wide")
-st.markdown("<h1 style='text-align: center; color: white;'>Shroom ID</h1>", unsafe_allow_html=True)
+st.set_page_config(page_title="Shroom ID", page_icon="./images/logo.ico", layout="wide")
+
+st.markdown("<h1 style='text-align: center;'>Shroom ID</h1>", unsafe_allow_html=True)
 st.markdown("<h6 style='text-align: center; color: gray;'>made by sutibn</h6>", unsafe_allow_html=True)
 
 cx, cy, cz = st.columns(3)
 with cy:
-    st.image("./images/mushrooms.png")
+    st.image("./images/banner.png")
 
 colA, colB = st.columns(spec=[10, 0.5])
 tabA, tabB = st.tabs(['About', 'Run'])
@@ -21,24 +22,37 @@ with tabA:
     df = pd.read_csv('./data/test.csv')
     st.write("This app uses a machine learning model built from a **Random Forest** classifier that determines if a mushroom is dangerous to eat based on its physical attributes.") 
     st.write(df)
-    st.write("The table above showcases the testing set of the model's data, which includes the most significant features relating to **edibility**.")
+    st.write("The table above showcases the testing set of the model's data, which includes the most significant features that indicate a mushroom's **edibility**.")
 
     about = '''If the mushroom is classified under a **0**, then it is most likely edible and safe for consumption.  
-    If the mushroom is classified under a **1**, then it is potentially poisonous and should not be ingested.'''
+        If the mushroom is classified under a **1**, then it is potentially poisonous and should not be ingested.'''
     st.markdown(about)
 
 with tabB:
+    cx, cy= st.columns(2)
+    with cx:
+        st.image("./images/cap-shape.png", width=650)
+        st.caption("Cap shape")
+        st.image("./images/gill-spacing.png")
+        st.caption("Gill spacing")
+
+    with cy:
+        st.image("./images/cap-surface.png", width=650)
+        st.caption("Cap surface")
+        st.image("./images/ring-type.png", width=650)
+        st.caption("Ring type")
+
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
-        cap_shape   = st.selectbox('**Cap shape**',   ['bell', 'conical', 'convex', 'flat', 'knobbed', 'sunken'])
-        cap_surface = st.selectbox('**Cap surface**', ['fibrous', 'grooves', 'scaly', 'smooth'])
+        cap_shape   = st.selectbox('**Cap shape**',   ['convex', 'bell', 'conical', 'knobbed', 'flat', 'sunken'])
+        cap_surface = st.selectbox('**Cap surface**', ['smooth', 'fibrous', 'scaly', 'grooves'])
         cap_color   = st.selectbox('**Cap color**',   ['brown', 'buff', 'cinnamon', 'gray', 'green', 'pink', 'purple', 'red', 'white', 'yellow'])
     feature_input['cap shape']   = cap_shape
     feature_input['cap surface'] = cap_surface
     feature_input['cap color']   = cap_color
 
     with col2:
-        gill_spacing = st.selectbox('**Gill spacing**', ['close', 'crowded', 'distant'])
+        gill_spacing = st.selectbox('**Gill spacing**', ['crowded', 'close', 'distant'])
         gill_size    = st.selectbox('**Gill size**',    ['broad', 'narrow'])     
         gill_color   = st.selectbox('**Gill color**',   ['black', 'brown', 'buff', 'chocolate', 'gray', 'green', 'orange', 'pink', 'purple', 'red', 'white', 'yellow'])    
     feature_input['gill spacing'] = gill_spacing
@@ -54,9 +68,9 @@ with tabB:
     feature_input['stalk color above ring']   = stalk_color_above_ring
 
     with col4:
-        ring_type = st.selectbox('**Ring ype**', ['cobwebby', 'evanescent', 'flaring', 'large', 'none', 'pendant', 'sheathing', 'zone'])
-        bruises   = st.selectbox('**Bruises**',  ['bruises', 'no'])
-        odor      = st.selectbox('**Odor**',     ['almond', 'anise', 'creosote', 'fishy', 'foul', 'musty', 'none', 'pungent', 'spicy'])
+        ring_type = st.selectbox('**Ring type**', ['pendant', 'flaring', 'sheathing', 'evanescent', 'large', 'cobwebby', 'zone', 'none'])
+        bruises   = st.selectbox('**Bruises**',   ['bruises', 'no'])
+        odor      = st.selectbox('**Odor**',      ['almond', 'anise', 'creosote', 'fishy', 'foul', 'musty', 'none', 'pungent', 'spicy'])
     feature_input['ring type'] = ring_type
     feature_input['bruises']   = bruises
     feature_input['odor']      = odor
@@ -69,7 +83,7 @@ with tabB:
     feature_input['population']        = population
     feature_input['habitat']           = habitat
 
-    st.info("After specifying the mushroom's features, press the button below to determine its edibility.\n\n  **NOTE**: The result may not be completely accurate if the mushroom is hard to describe—seek confirmation with expert sources to be sure.")
+    st.info("After specifying the mushroom's features, press the button below to determine its edibility.\n\n  **NOTE**: The result may be inaccurate if the mushroom is hard to completely describe—seek confirmation with expert sources to be sure.")
     if st.button("**Submit**"):
         X_test     = dv.transform(feature_input)
         prediction = model.predict(X_test)
